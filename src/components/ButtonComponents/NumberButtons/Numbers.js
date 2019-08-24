bc138e9a88b17fe5e65ce67b8a8d5cd898c35100
Michael Williams <manicdesigner@gmail.com>
2019-08-24 13:32:11 -0400

    add comments back to code for easier grading

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 
+//import any components needed
+import { numbers } from '../../../data';
+//Import your array data to from the provided data file
 import NumberButton from './NumberButton';
 
-import { numbers } from '../../../data';
 const Numbers = props => {
+  // STEP 2 - add the imported data to state
   const [calcNumbers] = useState(numbers);
   const { setDisplayState } = props;
-  // STEP 2 - add the imported data to state
+
   return (
     <div>
+      {/* STEP 3 - Use .map() to iterate over your array data and return a button
+       component matching the name on the provided file. Pass
+       it any props needed by the child component*/}
       {calcNumbers.map((number, index) => {
         return (
           <NumberButton
