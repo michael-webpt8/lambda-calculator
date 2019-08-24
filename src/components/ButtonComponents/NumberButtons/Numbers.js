efb38efc414f43e7f4eaad44d751aad92f9ee131
Michael Williams <manicdesigner@gmail.com>
2019-08-24 18:17:16 -0400

    converts string number stype to a numerical version

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -8,13 +8,19 @@ import NumberButton from './NumberButton';
 const Numbers = props => {
   // STEP 2 - add the imported data to state
   const { setDisplayState, numberChars } = props;
-
+  const strToNums = numberChars.map(numChar => {
+    // converts string nums to numerical version.
+    if (numChar !== '.') {
+      return Number(numChar);
+    }
+    return numChar;
+  });
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-      {numberChars.map((number, index) => {
+      {strToNums.map((number, index) => {
         return (
           <NumberButton
             setDisplayState={setDisplayState}
