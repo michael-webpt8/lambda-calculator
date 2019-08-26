0b602b2f0242f6cb2c4085a4d23d6cce269af80f
Michael Williams <manicdesigner@gmail.com>
2019-08-26 06:17:29 -0400

    got rid of number conversion abrupt

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -8,19 +8,13 @@ import NumberButton from './NumberButton';
 const Numbers = props => {
   // STEP 2 - add the imported data to state
   const { setDisplayState, number } = props;
-  const strToNums = number.map(numChar => {
-    // converts string nums to numerical version.
-    if (numChar !== '.') {
-      return Number(numChar);
-    }
-    return numChar;
-  });
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-      {strToNums.map((num, index) => {
+      {number.map((num, index) => {
         return (
           <NumberButton
             setDisplayState={setDisplayState}
